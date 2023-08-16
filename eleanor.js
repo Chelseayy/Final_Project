f7cdf923dce4a3e45e1749eedda34e45bcd56f45
Chelseayy <134656980+Chelseayy@users.noreply.github.com>
2023-08-16 15:55:48 +1000

    Add files via upload

--- eleanor.js
+++ eleanor.js
@@ -1,13 +1,13 @@
 function showImage(imageUrl) {
-    var selectedImagesDiv = document.getElementById("selected-images");
-    var imageElement = document.createElement("div");
-    imageElement.className = "selected-image";
-    imageElement.innerHTML = '<img src="' + imageUrl + '" alt="Selected Image">' +
-                              '<div class="delete-icon" onclick="removeImage(this)">X</div>';
-    selectedImagesDiv.appendChild(imageElement);
-  }
-  
-  function removeImage(deleteIcon) {
-    var imageElement = deleteIcon.parentNode;
-    imageElement.parentNode.removeChild(imageElement);
-  }
+  var selectedImagesDiv = document.getElementById("selected-images");
+  var imageElement = document.createElement("div");
+  imageElement.className = "selected-image";
+  imageElement.innerHTML = '<img src="' + imageUrl + '" alt="Selected Image">' +
+                            '<div class="delete-icon" onclick="removeImage(this)">X</div>';
+  selectedImagesDiv.appendChild(imageElement);
+}
+
+function removeImage(deleteIcon) {
+  var imageElement = deleteIcon.parentNode;
+  imageElement.parentNode.removeChild(imageElement);
+}
